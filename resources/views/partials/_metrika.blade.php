b77c605cdd099d61ba1293c04ebe56d6e17cff30
jambik <jambik@gmail.com>
2016-06-02 22:01:13 +0300

    - changes

--- resources/views/partials/_metrika.blade.php
+++ resources/views/partials/_metrika.blade.php
@@ -1,3 +1,18 @@
 @if (app()->environment('production'))
-
+    <!-- Yandex.Metrika counter -->
+    <script src="https://mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        try {
+            var yaCounter37722990 = new Ya.Metrika({
+                id:37722990,
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                trackHash:true
+            });
+        } catch(e) { }
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/37722990" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 @endif
