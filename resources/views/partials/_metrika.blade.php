bd352aaeccfe9c3f8eee7b94ed15f83cee97457c
jambik <jambik@gmail.com>
2016-12-07 22:43:50 +0300

    - changes

--- resources/views/partials/_metrika.blade.php
+++ resources/views/partials/_metrika.blade.php
@@ -1,18 +1,29 @@
 @if (app()->environment('production'))
     <!-- Yandex.Metrika counter -->
-    <script src="https://mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
     <script type="text/javascript">
-        try {
-            var yaCounter37722990 = new Ya.Metrika({
-                id:37722990,
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true,
-                trackHash:true
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter40967639 = new Ya.Metrika({
+                        id:40967639,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true
+                    });
+                } catch(e) { }
             });
-        } catch(e) { }
+            var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
     </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/37722990" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <noscript><div><img src="https://mc.yandex.ru/watch/40967639" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
 @endif
